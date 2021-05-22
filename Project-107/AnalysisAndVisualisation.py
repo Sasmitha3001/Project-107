0b2afacbcbfbc18fbc50dc92956a0bf42a13d14a
Sasmitha3001 <65769282+Sasmitha3001@users.noreply.github.com>
2021-05-22 16:10:54 +0530

    Update AnalysisAndVisualisation.py

--- Project-107/AnalysisAndVisualisation.py
+++ Project-107/AnalysisAndVisualisation.py
@@ -3,8 +3,10 @@ import plotly.express as px
 import csv 
 
 data=pd.read_csv('data.csv')
-df=data.groupby('level')['attempt'].mean()
-print(df)
+df=data.groupby(['student_id','level'],as_index=False)['attempt'].mean()
+# print(df)
+# level=df.loc[df['level']]
+# print(level)
 
-fig=px.scatter(x=df,y='level',color='attempt',size='level')
+fig=px.scatter(df,x='student_id',y='level',color='attempt',size='attempt')
 fig.show()
